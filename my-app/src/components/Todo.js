decdce89488631974d05a0e4ad6c5ff78699e1e3
Blanca Chacon <Bchacon450@gmail.com>
2023-08-10 15:55:10 +0200

    3
    3

---
+++ my-app/src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Todo = () => {
+    return(
+        <div className="todo">
+            <li className='todo-item'>heye</li>
+            
+        </div>
+    );
+}
+
+export default Todo;
