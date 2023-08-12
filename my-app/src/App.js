179093ebacb5f5668ae91a08cab74f49cf7b6de4
Blanca Chacon <Bchacon450@gmail.com>
2023-08-12 11:01:22 +0200

    01 12.08
    01  => 12.08

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 //Importing Components
@@ -6,17 +6,63 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  //State stuff
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //Run once whem the app start
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+    //Use effect
+    useEffect(() => {
+      filterHandler();
+      saveLocalTodos();
+    }, [todos, status]);
+  
+
+  //Functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+
+  // Save to local
+  let saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  let getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null){
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
+    }
+  };
+
+
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
-      <TodoList setTodos={setTodos} todos={todos}/>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus} />
+      <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos}/>
     </div>
   );
-}
+};
 
 export default App;
